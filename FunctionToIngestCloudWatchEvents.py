e5c1c500ac60621577bb17f23a1676b07638f86a
Sumit Bajaj <sumit.bajaj@mistsys.com>
2018-06-15 16:10:27 -0700

    Support for logging user information added

--- FunctionToIngestCloudWatchEvents.py
+++ FunctionToIngestCloudWatchEvents.py
@@ -3,11 +3,12 @@ import boto3
 import json
 import botocore
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
 DOREMEDY = True
-debug_enabled = True
+#Debug Control, set to True for logs to be oserved in CloudWatch
+debug_enabled = False
+LOGTABLE = "CloudTrailLogEventData"
+# Boolean for Test
+TEST = True
 
 # Security Group API Calls to Log
 SECURITY_GROUP_CHANGE_APIS = ["AuthorizeSecurityGroupIngress","AuthorizeSecurityGroupEgress",
@@ -81,13 +82,15 @@ def priority_notification(event, eventName):
 
 
 def remediate_security_group_change(event):
+    
     group_id = event["detail"]["requestParameters"]["groupId"]
     client = boto3.client("ec2");
     
     try:
         response = client.describe_security_groups(GroupIds=[group_id])
     except botocore.exceptions.ClientError as e:
-        logger.error(e)
+        print("Error",e)
+        return False
 
     ip_permissions = response["SecurityGroups"][0]["IpPermissions"]
     authorize_permissions = [item for item in REQUIRED_PERMISSIONS if item not in ip_permissions]
@@ -117,6 +120,127 @@ def remediate_cloudtrail_change(event):
         response = client.start_logging(Name=trailArn)
         return True
 
+def verifyLogTable():
+    """Verifies if the table name provided is deployed using CloudFormation
+       template and thereby have a prefix and suffix in the name.
+    Returns:
+        The real table name
+        TYPE: String
+    """
+    client = boto3.client('dynamodb')
+    resource = boto3.resource('dynamodb')
+    table = LOGTABLE
+
+    response = client.list_tables()
+    tableFound = False
+    for n, _ in enumerate(response['TableNames']):
+        if table in response['TableNames'][n]:
+            table = response['TableNames'][n]
+            tableFound = True
+
+    if not tableFound:
+        # Table not created in CFn, let's check exact name or create it
+        try:
+            result = client.describe_table(TableName=table)
+        except:
+            # Table does not exist, create it
+            newtable = resource.create_table(
+                TableName=table,
+                KeySchema=[
+                    {'AttributeName': 'userName', 'KeyType': 'HASH'},
+                ],
+                AttributeDefinitions=[
+                    {'AttributeName': 'userName', 'AttributeType': 'S'},
+                ],
+                ProvisionedThroughput={'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5}
+            )
+            # Wait for table creation
+            newtable.meta.client.get_waiter('table_exists').wait(TableName=table)
+    return table
+
+def sendAlert(data):
+    """Placeholder for alert functionality.
+       This could be Amazon SNS, SMS, Email or adding to a ticket tracking
+       system like Jira or Remedy.
+    Args:
+        data (dict): All extracted event info.
+    Returns:
+        TYPE: String
+    """
+    return 0
+
+def logEvent(logData, table):
+    """Log all information to the provided DynamoDB table.
+    Args:
+        logData (dict): All extracted information
+        table (string): Table name for event history.
+    Returns:
+        TYPE: Success
+    """
+    client = boto3.client('dynamodb')
+
+    #Store data
+    response = client.put_item(
+        TableName=table,
+        Item={
+            'userName': {'S': logData['userName']},
+            'eventTime': {'S': logData['eventTime']},
+            'userArn': {'S': logData['userArn']},
+            'region': {'S': logData['region']},
+            'account': {'S': logData['account']},
+            'userAgent': {'S': logData['userAgent']},
+            'sourceIP': {'S': logData['sourceIP']}
+        }
+    )
+    return 0
+
+def forensic(data, table):
+    """Perform forensic on the resources and details in the event information.
+       Example: Look for MFA, previous violations, corporate CIDR blocks etc.
+    Args:
+        data (dict): All extracted event info.
+        table (string): Table name for event history.
+    Returns:
+        TYPE: String
+    """
+    # Set remediationStatus to True to trigger remediation function.
+    remediationStatus = False
+
+    if remediationStatus:
+        # See if user have tried this before.
+        client = boto3.client('dynamodb')
+        response = client.get_item(
+            TableName=table,
+            Key={
+                'userName': {'S': data['userName']}
+            }
+        )
+        try:
+            if response['Item']:
+                # If not first time, trigger countermeasures.
+                result = disableAccount(data['userName'])
+                return result
+        except:
+            # First time incident, let it pass.
+            return "NoRemediationNeeded"
+
+
+def disableAccount(userName):
+    """Countermeasure function that disables the user by applying an
+       inline IAM deny policy on the user.
+       policy.
+    Args:
+        userName (string): Username that caused event.
+    Returns:
+        TYPE: Success
+    """
+    client = boto3.client('iam')
+    response = client.put_user_policy(
+        UserName=userName,
+        PolicyName='BlockPolicy',
+        PolicyDocument='{"Version":"2012-10-17", "Statement":{"Effect":"Deny", "Action":"*", "Resource":"*"}}'
+    )
+    return 0
 
 def lambda_handler(event, context):
     """Summary
@@ -126,14 +250,12 @@ def lambda_handler(event, context):
     Returns:
         TYPE: Description
     """
-    #Debug Control, set to True for logs to be oserved in CloudWatch
+    
     
     #Begin evaluating event
     eventName = event['detail']['eventName']
     
-    # test:
-    print("test")
-    print(event['detail']['requestParameters'])
+    #Test:
     
     #priority to check notification type and remediate
     notification_type, remedyDone = priority_notification(event,eventName)
@@ -159,5 +281,16 @@ def lambda_handler(event, context):
                'userAgent': userAgent, 'sourceIP': sourceIP, "eventSource":eventSource,
               'notification':notification_type, 'remedyDone':remedyDone}
     
-    #log to cloudwatch
+    #Alerting
+    # result = sendAlert(logData)
+
+    #TODO:Temporary to send to ElasticSearch, Required Minimal Calls.
     print(json.dumps(logData))
+    
+    # Forensics
+    realTable = verifyLogTable()
+    # result = forensic(logData, realTable)
+
+    # Logging
+    result = logEvent(logData, realTable)
+    return result
