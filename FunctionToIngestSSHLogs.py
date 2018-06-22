4b56e9e3ca0ae82158ee6eaf9b6044000bba389c
Sumit Bajaj <sumit.bajaj@mistsys.com>
2018-06-22 16:37:10 -0700

    Added Log Type Details

--- FunctionToIngestSSHLogs.py
+++ FunctionToIngestSSHLogs.py
@@ -15,28 +15,46 @@ logging.getLogger().setLevel(logging.INFO)
 import re
 time_fmt = '%Y-%m-%d %H:%M:%S'
 
-# TODO: Handle API Calls and Build Cached Solution For Queries
-ip_lookup = {"city":None,"region":None,"location": None,"country":None,"geoname":None}
+#TODO: Handle API Calls and Build Cached Solution For Queries
+geoip  = {"city_name":None,"region_name":None,"location": None,
+          "country_name":None,"latitude":None,"longitude":None}
 
-def get_ip_info(address):
+def getIPInfo(address):
+    """Returns a geoip type object that countains city_name, region_name,
+    location, latutute, longitude, and country_name for a given IP address
+    looked up over 
+    Args:
+        address:IP address
+        TYPE:String
+    Returns:
+        geoip dictionary
+        TYPE: Dictionary
+    """
     api = "http://api.ipstack.com/"
     access_key="?access_key=__IPSTACK_KEY__&output=json"
     try:
         request_string=api+address+access_key
         response = requests.get(api+address+access_key)
     except:
-        return ip_lookup
+        return geoip 
 
     if (response.status_code==200):
         json_response = json.loads(response.text)
-        ip_lookup["city"]=str(json_response["city"])
-        ip_lookup["region"]=str(json_response["region_name"])
-        ip_lookup["location"]={"lat": json_response["latitude"],"lon": json_response["longitude"]}
-        ip_lookup["country"]=str(json_response["country_name"])
-        ip_lookup["geoname"]=str(json_response["location"]["geoname_id"])
-    return ip_lookup
+        geoip ["city_name"]=str(json_response["city"])
+        geoip ["region_name"]=str(json_response["region_name"])
+        geoip ["location"]=[json_response["longitude"],json_response["latitude"]]
+        geoip ["latitude"]=json_response["latitude"]
+        geoip ["longitude"]=json_response["longitude"]
+        geoip ["country_name"]=str(json_response["country_name"])
+    return geoip 
     
 def lambda_handler(event, context):
+    """Summary
+    Processes events subscribed from /var/secure logs from EC2 Instances
+    Args:
+        event (TYPE): Description
+        context (TYPE): Description
+    """
     #CloudWatch log data
     outEvent = str(event['awslogs']['data'])
     
@@ -49,13 +67,11 @@ def lambda_handler(event, context):
     logData = {"notification":"EC2_AUTH_LOGS","source":None, "isSSHLog":False,"logType":None}
     
     for logEvent in cleanEvent['logEvents']:
-        # print("Event:",json.dumps(logEvent))
-        source=logEvent['extractedFields']['process']
-        logData['source']=source
-        message=logEvent['extractedFields']['eventdata']
+        logData['source']=logEvent['extractedFields']['source']
+        message=logEvent['extractedFields']['message']
         logData['eventTimestamp']=logEvent['timestamp']
-        # print("Message:",logEvent['message'])
-        if "sshd" in source:
+        utility=logEvent['extractedFields']['utility']
+        if "sshd" in utility:
             logData['isSSHLog']=True
             if "session opened" in message:
                 logData['logType']="SuccessfulLogin"
@@ -70,22 +86,25 @@ def lambda_handler(event, context):
                 logData['logType']="FailedLogin"
                 logData['user']=re.findall(r"user(.*?)from",message)[0].strip()
                 logData['IP']=re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b",message)[0]
-                logData['FailedLoginInfo']=get_ip_info(logData['IP'])  
+                logData['geoip']=getIPInfo(logData['IP'])  
             elif "maximum authentication attempts" in message:
                 logData['logType']="MaxAuthTries"
                 logData['user']=re.findall(r"user(.*?)from",message)[0]
                 logData['IP']=re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b",message)[0]
-                logData['DisconnectedInfo']=get_ip_info(logData['IP'])            
+                logData['geoip']=getIPInfo(logData['IP'])            
             else:
                 continue
-        elif "sudo" in source:
+        elif "sudo" in utility:
             logData['isSSHLog']=False
             logData['SudoInfo']=message
-        elif "runuser" in source:
+            logData['logType']="sudo"
+        elif "runuser" in utility:
             logData['isSSHLog']=False
             logData['RunUserInfo']=message
+            logData['logType']="runuser"
         else:
             logData['isSSHLog']=False
             logData['DefaultInfo']=message
+        #print logData to be consumed by ElasticSearch
         print(json.dumps(logData))
         
